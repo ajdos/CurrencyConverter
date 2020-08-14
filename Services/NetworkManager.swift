0b879ea82ff17da9ea187e2893f011298752253a
Aidin <aidin.abdurakhmanov@gmail.com>
2020-08-14 16:46:56 +0300

    add file network manager

---
+++ Services/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  CurrencyConverter
+//
+//  Created by Айдин Абдурахманов on 14.08.2020.
+//  Copyright © 2020 Айдин Абдурахманов. All rights reserved.
+//
+
+import Foundation
